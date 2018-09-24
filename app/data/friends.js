6a4f8e60578dc85adb760b77da972506834163ec
Tomazicon <dave.tomazin@gmail.com>
2018-09-23 18:03:09 -0600

    final

---
+++ app/data/friends.js
@@ -0,0 +1,55 @@
+var friends = [
+    {
+        name: "Yosemite Sam",
+        photo: "http://images5.fanpop.com/image/photos/30900000/Yosemite-Sam-warner-brothers-animation-30976317-600-591.jpg",
+        scores: [
+            "5",
+            "1",
+            "4",
+            "4",
+            "5",
+            "1",
+            "2",
+            "5",
+            "4",
+            "1"
+      ]
+
+    }, 
+    {
+        name: "Rosey",
+        photo: "http://mchiaviello.com/wp-content/uploads/2015/08/Aug26-600x344.jpg",
+        scores: [
+            "4",
+            "1",
+            "3",
+            "4",
+            "5",
+            "2",
+            "2",
+            "5",
+            "4",
+            "1"
+      ]
+
+    }, 
+    {
+        name: "Optimus Prime",
+        photo: "http://www.pajiba.com/image/optimus1.jpg",
+        scores: [
+            "3",
+            "1",
+            "5",
+            "4",
+            "5",
+            "4",
+            "2",
+            "5",
+            "4",
+            "2"
+      ]
+
+    }, 
+]
+
+module.exports = friends; 
